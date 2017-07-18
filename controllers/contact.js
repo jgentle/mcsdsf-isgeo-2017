c41b7502e25bfd68058d721bfffa90b86d4d3c6f
taoteg <taoteg@gmail.com>
2017-07-18 16:48:50 -0500

    Some minor tweaks to packages.json to reflect project correctly and to COntact form. Switched to new MongoDB for this application. Now to kick tires.

--- controllers/contact.js
+++ controllers/contact.js
@@ -37,7 +37,7 @@ exports.postContact = (req, res) => {
   const mailOptions = {
     to: 'your@email.com',
     from: `${req.body.name} <${req.body.email}>`,
-    subject: 'Contact Form | Hackathon Starter',
+    subject: 'Contact Form | MCSDSF IS-GEO 2017',
     text: req.body.message
   };
 
