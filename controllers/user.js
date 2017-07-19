98b629b4761afaf30832391b09cb5973843003d4
taoteg <taoteg@gmail.com>
2017-07-18 22:08:40 -0500

    Tweaked templates to reflect layout desired. Ned to integrate Vue.js and D3, leaflet, crossfilter interactives now.

--- controllers/user.js
+++ controllers/user.js
@@ -355,8 +355,8 @@ exports.postForgot = (req, res, next) => {
     });
     const mailOptions = {
       to: user.email,
-      from: 'hackathon@starter.com',
-      subject: 'Reset your password on Hackathon Starter',
+      from: 'jgentle@tacc.utexas.edu',
+      subject: 'Reset your password on MCSDSF IS-GEO 2017',
       text: `You are receiving this email because you (or someone else) have requested the reset of the password for your account.\n\n
         Please click on the following link, or paste this into your browser to complete the process:\n\n
         http://${req.headers.host}/reset/${token}\n\n
