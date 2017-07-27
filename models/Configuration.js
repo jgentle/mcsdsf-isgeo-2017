659277baa661f0510bfd999cdf9ad6e6f1761a1f
taoteg <taoteg@gmail.com>
2017-07-26 19:18:51 -0500

    Refactored Data Models significntly to account for variations in possible configurations and to better incapsulate responsibilities within entities.

--- models/Configuration.js
+++ models/Configuration.js
@@ -3,6 +3,8 @@ const Schema = mongoose.Schema;
 
 const configurationSchema = new mongoose.Schema({
   name: String,
+  dataSource_id: { type: Schema.Types.ObjectId, ref: 'DataSource' },   // _id for document in datasources collection.
+  geodataSource_id: { type: Schema.Types.ObjectId, ref: 'DataSource' },   // _id for document in datasources collection.
   configurationData_id: { type: Schema.Types.ObjectId, ref: 'JsonData' }   // _id for document in sourcedata collection.
 }, { timestamps: true });
 
--- models/Formulation.js
+++ models/Formulation.js
@@ -4,10 +4,9 @@ const Schema = mongoose.Schema;
 const formulationSchema = new mongoose.Schema({
   name: String,
   description: String,
-  dataSource_id: { type: Schema.Types.ObjectId, ref: 'DataSource' },   // _id for document in datasources collection.
-  geodataSource_id: { type: Schema.Types.ObjectId, ref: 'DataSource' },   // _id for document in datasources collection.
   systemsModel_id: { type: Schema.Types.ObjectId, ref: 'SystemsModel' },   // _id for document in datasources collection.
   configuration_id: { type: Schema.Types.ObjectId, ref: 'Configuration' },   // _id for document in configuration collection.
+  isValidated: Boolean,   // check to ensure all sub-components have been validated.
   insights: [{ type: Schema.Types.ObjectId, ref: 'Insight' }]   // Array of subdocument _ids for documents in the insights collection.
 }, { timestamps: true });
 
